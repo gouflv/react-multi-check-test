7ba15921d52c8fe40951e13fc4274a64a4797b8a
gouflv <lv.gouf@gmail.com>
2021-04-14 12:45:52 +0800

    test(styled-helper): prop

--- src/utils/styled-helper.ts
+++
@@ -1,8 +0,0 @@
-export function prop<P extends Record<string, string>>(
-  key: string,
-  defaultValue: string
-) {
-  return function (props: P): P[keyof P] | string {
-    return typeof props[key] !== 'undefined' ? props[key] : defaultValue;
-  };
-}
