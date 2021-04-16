9a9178cbac31f009e17f7a99a4b83f483e833dc6
gouflv <lv.gouf@gmail.com>
2021-04-16 11:34:58 +0800

    chore(webpack): use source-map devtool for performance

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/entry.tsx',
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
