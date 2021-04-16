791f68d1287de27d104ac5bd26a8ad1abce0ec73
gouflv <lv.gouf@gmail.com>
2021-04-16 11:18:28 +0800

    refactor(useSet): change hook return value typing

--- src/hooks/useSet.ts
+++ src/hooks/useSet.ts
@@ -1,15 +1,28 @@
 import {useCallback, useMemo, useState} from 'react';
 
-const useSet = <T>(
-  initialValue = new Set<T>()
-): {
-  value: Set<T>;
-  actions: {
-    add: (item: T) => void;
-    remove: (item: T) => void;
-    has: (item: T) => boolean;
-  };
-} => {
+type Actions<T> = {
+  add: (item: T) => void;
+  remove: (item: T) => void;
+  has: (item: T) => boolean;
+};
+
+/**
+ * React state hook that tracks a Set.
+ *
+ * Do not use the actions object directly, it's not been memoized.
+ *
+ * @param {Set} initialValue
+ * @return {[ Set, Actions ]}
+ *
+ * @example
+ *
+ * // Right
+ * const [value, { add, remove, has }] = useSet(new Set(['foo', 'bar']))
+ *
+ * // Wrong
+ * const [value, actions] = useSet()
+ */
+const useSet = <T>(initialValue = new Set<T>()): [Set<T>, Actions<T>] => {
   const [value, setValue] = useState(initialValue);
 
   const setters = useMemo(() => {
@@ -30,13 +43,13 @@ const useSet = <T>(
     [value]
   );
 
-  return {
+  return [
     value,
-    actions: {
+    {
       ...setters,
       has
     }
-  };
+  ];
 };
 
 export default useSet;
